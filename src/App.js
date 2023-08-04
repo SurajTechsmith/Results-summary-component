da861cc86e8a6cf770355574133e376180ddf36a
Suraj <surajyadav27092001@gmail.com>
2023-08-04 06:06:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
 
 function App() {
+  
+
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const colorsArray = [
+    { name: 'Light red', hsl: 'hsl(0, 100%, 67%)' },
+    { name: 'Orangey yellow', hsl: 'hsl(39, 100%, 56%)' },
+    { name: 'Green teal', hsl: 'hsl(166, 100%, 37%)' },
+    { name: 'Cobalt blue', hsl: 'hsl(234, 85%, 45%)' }
+  ];
+  const BGcolorsArray = [
+    { name: 'Light red', hsl: 'hsl(0, 100%, 97%)' },
+    { name: 'Orangey yellow', hsl: 'hsl(39, 100%, 96%)' },
+    { name: 'Green teal', hsl: 'hsl(166, 100%, 97%)' },
+    { name: 'Cobalt blue', hsl: 'hsl(234, 85%, 95%)' }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="container">
+        <div className="part-1">
+        <h4 style={{
+    color: 'hsl(221, 100%, 96%)'
+}}>Your Result</h4>
+
+          <div className="circle">
+          <h1>76</h1>
+          <p>of 100</p>
+          </div>
+          <p>Great</p>
+          <p className='para'>you have scored higher than 65% of the people who have taken these test.</p>
+
+         
+        </div>
+        <div className="part-2">
+      <h4>Summary</h4>
+      {data.map((item, index) => (
+        <div key={index} className='part-2-item' style={{ background: BGcolorsArray[index].hsl }}>
+        <div className='part-im'>
+          <img src={item.icon} alt={`${item.category} Icon`} />
+          <p style={{ color: colorsArray[index].hsl }}> {item.category}</p>
+          </div>
+           <p>
+
+            <span style={{ color: 'black' }}>{item.score}</span>/100
+          </p>
+
+        </div>
+      ))}
+      <button className='btn'>Continue</button>
+
     </div>
+
+      </div>
+    </>
   );
 }
 
